f29a228ac70c1a6f6cdc231d9e1ee6d42c8671d6
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-05-13 15:18:02 -0300

    feat: add cli base

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,45 @@
 pub mod ast;
 
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Args, Debug, Clone)]
+pub struct RenameArgs {}
+
+#[derive(Args, Debug, Clone)]
+pub struct SearchArgs {}
+
+#[derive(Args, Debug, Clone)]
+pub struct EvalArgs {
+    /// Enables the verbose mode on Command Line Interface.
+    #[clap(short = 'v', long, default_value = "auto")]
+    pub verbose: bool,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+pub enum Command {
+    Rename(RenameArgs),
+    Search(SearchArgs),
+    Eval(EvalArgs),
+}
+
+pub fn run_cli() {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Command::Rename(..) => todo!(),
+        Command::Search(..) => todo!(),
+        Command::Eval(..) => todo!(),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    run_cli();
 }
