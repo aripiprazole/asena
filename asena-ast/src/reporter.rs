f3b9721ee51a000e46853efcd0785448478eee20
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-07-14 20:55:59 +0000

    feat(asena-ast): add api for declarations

--- asena-ast/src/reporter.rs
+++ asena-ast/src/reporter.rs
@@ -26,6 +26,19 @@ impl Clone for Reporter {
     }
 }
 
+pub struct ContextBuilder {
+    pub contexts: Vec<Spanned<BoxInternalError>>,
+}
+
+impl ContextBuilder {
+    pub fn push<E: InternalError + 'static>(&mut self, at: &dyn Located, error: E) {
+        let item = Spanned::new(at.location().into_owned(), ());
+        let error = BoxInternalError(Rc::new(error));
+
+        self.contexts.push(item.replace(error));
+    }
+}
+
 impl Reporter {
     pub fn new(src: &str, tree: Spanned<Tree>) -> Self {
         Self {
@@ -37,6 +50,22 @@ impl Reporter {
 
     pub fn report<E: InternalError + 'static, T: Located>(&self, at: &T, error: E) {
         self.diagnostic(Spanned::new(at.location().into_owned(), ()), error);
+    }
+
+    pub fn report_with<F, E: InternalError + 'static, T: Located>(&self, at: &T, error: F)
+    where
+        F: FnOnce(&mut ContextBuilder) -> E,
+    {
+        let mut builder = ContextBuilder { contexts: vec![] };
+        let error = error(&mut builder);
+        let item = Spanned::new(at.location().into_owned(), ());
+        let mut diagnostic = Diagnostic::new(item.replace(BoxInternalError(Rc::new(error))));
+
+        for context in builder.contexts {
+            diagnostic = diagnostic.add_child(context);
+        }
+
+        self.errors.lock().unwrap().insert(diagnostic);
     }
 
     pub fn diagnostic<E: InternalError + 'static, T>(&self, at: Spanned<T>, error: E) {
--- asena-report/src/lib.rs
+++ asena-report/src/lib.rs
@@ -5,10 +5,14 @@ use std::path::PathBuf;
 use std::rc::Rc;
 use std::{error::Error, fmt::Display};
 
+use ariadne::{Color, Config, LabelAttach};
 use asena_leaf::node::Tree;
 use asena_span::{Loc, Spanned};
 
+pub use ide_diagnostic::*;
 pub use Fragment::*;
+
+pub mod ide_diagnostic;
 
 pub trait InternalError: Error {
     fn code(&self) -> u16 {
@@ -72,64 +76,6 @@ pub struct Report<T: InternalError> {
     pub diagnostics: Vec<Diagnostic<T>>,
 }
 
-#[derive(Debug, Clone)]
-pub struct Diagnostic<T: InternalError> {
-    pub kind: DiagnosticKind,
-    pub code: u16,
-    pub message: Spanned<T>,
-    pub quickfixes: Vec<Quickfix>,
-    pub children: Vec<Diagnostic<T>>,
-}
-
-impl<T: InternalError> Eq for Diagnostic<T> {}
-
-impl<T: InternalError> PartialEq for Diagnostic<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.kind == other.kind
-            && self.code == other.code
-            && self.message.span == other.message.span
-            && self.message.value.code() == other.message.value.code()
-            && self.message.value.kind() == other.message.value.kind()
-            && self.message.value.to_string() == other.message.value.to_string()
-            && self.quickfixes == other.quickfixes
-            && self.children == other.children
-    }
-}
-
-impl<T: InternalError> Hash for Diagnostic<T> {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        let message = (
-            &self.message.span,
-            self.message.value.code(),
-            self.message.value.kind(),
-        );
-
-        self.kind.hash(state);
-        self.code.hash(state);
-        message.hash(state);
-        self.quickfixes.hash(state);
-        self.children.hash(state);
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
-#[repr(C)]
-pub enum DiagnosticKind {
-    Error = 1,
-    HardError = 2,
-    InternalError = 3,
-    Warning = 4,
-    Deprecated = 5,
-    Info = 6,
-    Tip = 7,
-    Meta = 8,
-    SyntaxError = 9,
-    TypeError = 11,
-    ResolutionError = 12,
-    Lint = 13,
-    LoweringError = 14,
-}
-
 impl<E: InternalError> Report<E> {
     pub fn new(source: &str, tree: Spanned<Tree>) -> Self {
         Self {
@@ -163,88 +109,6 @@ impl<E: InternalError> Report<E> {
         for diagnostic in self.diagnostics.iter() {
             diagnostic.dump(&self.source);
         }
-    }
-}
-
-impl<E: InternalError> Diagnostic<E> {
-    pub fn new(error: Spanned<E>) -> Self {
-        Self {
-            kind: error.kind(),
-            code: error.code(),
-            message: error,
-            children: vec![],
-            quickfixes: vec![],
-        }
-    }
-
-    pub fn add_fixes(mut self, fixes: Vec<Quickfix>) -> Self {
-        self.quickfixes.extend(fixes);
-        self
-    }
-
-    pub fn add_child(mut self, message: Spanned<E>) -> Self {
-        self.children.push(Diagnostic {
-            kind: message.kind(),
-            code: message.code(),
-            message,
-            children: vec![],
-            quickfixes: vec![],
-        });
-
-        self
-    }
-
-    fn as_label(&self, colors: &mut ariadne::ColorGenerator) -> ariadne::Label {
-        ariadne::Label::new(self.message.span.clone().into_ranged().unwrap_or_default())
-            .with_message(self.message.value.to_string())
-            .with_color(match self.kind {
-                DiagnosticKind::Warning | DiagnosticKind::Deprecated => ariadne::Color::Yellow,
-                DiagnosticKind::Info => ariadne::Color::Blue,
-                DiagnosticKind::HardError
-                | DiagnosticKind::Error
-                | DiagnosticKind::InternalError => ariadne::Color::Red,
-                _ => colors.next(),
-            })
-    }
-
-    fn dump(&self, source: &str)
-    where
-        E: Clone,
-    {
-        use ariadne::{ColorGenerator, Report, ReportKind, Source};
-
-        let mut builder = Report::<Range<usize>>::build(ReportKind::Error, (), 0)
-            .with_code(format!("E{:03X}", self.code))
-            .with_message(self.message.value().to_string());
-        let mut colors = ColorGenerator::new();
-        let mut children = vec![];
-        children.push(self.clone());
-        children.extend(self.children.clone());
-
-        builder = builder.with_labels(
-            children
-                .iter()
-                .map(|diagnostic| diagnostic.as_label(&mut colors)),
-        );
-        if !self.quickfixes.is_empty() {
-            let mut fixes = vec![];
-            for fix in &self.quickfixes.clone() {
-                let loc = fix.loc.clone();
-                let message = fix
-                    .message
-                    .iter()
-                    .map(|x| x.to_string())
-                    .collect::<Vec<_>>()
-                    .join(", ");
-
-                fixes.push(format!("{message} at {loc}"));
-            }
-            builder = builder.with_help(format!("Can be fixed by: {}", fixes.join("; ")))
-        }
-        builder
-            .finish()
-            .print(Source::from(source.clone()))
-            .unwrap();
     }
 }
 
